cdcf4681c385969d9834bcfaf066726a806a5136
Litvinova Galina <litvinovaga@inbox.ru>
2023-07-27 03:24:58 +0300

    Правки отправки сообщений

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -5,17 +5,38 @@ import { ChatPageProps } from './types';
 import Block from '../../components/block/block';
 import ChatFeed from '../../components/chatFeed/chatFeed';
 import MessageHistoryBlock from '../../components/messageHistory/messageHistory';
+import Button from '../../components/button/button';
+import { MessageValidator } from '../../validators/messageValidator';
 export default class ChatPage extends Block<ChatPageProps> {
     constructor() {
         const props = {
             chatFeed: new ChatFeed(),
             history: new MessageHistoryBlock(),
             send: sendPic,
+            sendButton: new Button('div', {                
+                text: 'Send',
+                events: {
+                    'click': (e: MouseEvent) => {this.sendMessage(e);}
+                }
+            }),
             attributes: {
                 class: 'chat'
             }
         };
         super('div', props);
+    }
+    sendMessage(e: MouseEvent) {
+        const text = this._element.querySelector('textarea');
+        const validator = new MessageValidator();
+        if (validator.isValid(text?.value || '')){
+            text?.setCustomValidity('');
+            console.log(text?.value);
+        }
+        else {
+            text?.setCustomValidity(validator.getMessage());
+        }
+        text?.reportValidity();
+        e.stopPropagation();
     }
     render() {
         return this.compile(chat);
